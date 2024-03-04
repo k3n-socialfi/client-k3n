7deea4057b3be37901870c14e1b40c6ceb8d1412
curtis <curtisnguyen4@gmail.com>
2024-03-04 18:33:36 +0700

    config context alert

--- src/components/alerts/AlertsBasic.tsx
+++
@@ -1,9 +0,0 @@
-// ** MUI Imports
-import Alert from "@mui/material/Alert";
-
-export default function InputAlertsBasic({ children, ...props }: any){
-  return (
-      <Alert {...props}>{children}</Alert>
-  );
-};
-
