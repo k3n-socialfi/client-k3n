3c6b636886038d1a6bc36f8e6a4162cffe2a4e70
curtis <curtisnguyen4@gmail.com>
2024-03-05 22:42:52 +0700

    fix: build component button

--- src/components/ButtonCustom/style.ts
+++ src/components/ButtonCustom/style.ts
@@ -5,41 +5,35 @@ export const StyleButton = styled(Button)<ICustomButtonProps>`
   border-radius: 50px;
   text-transform: inherit;
   color: ${(props: any) =>
-    props.theme.colors.button[props.colorBt] ??
-    props.theme.colors.button.white};
+    props.theme.colors[props.colorBt] ?? props.theme.colors.whiteText};
   border-color: ${(props: any) =>
-    props.theme.colors.button[props.borderColorBt] ??
-    props.theme.colors.button.white};
+    props.theme.colors[props.borderColorBt] ?? props.theme.colors.whiteText};
   background-color: ${(props: any) =>
-    props.theme.backgrounds.button[props.backgroundColorBt] ??
-    props.theme.backgrounds.button.primaryEnabled};
+    props.theme.colors[props.backgroundColorBt] ??
+    props.theme.colors.primaryEnabledBt};
   &:hover {
     color: ${(props: any) =>
-      props.theme.colors.button[props.colorBtHover] ??
-      props.theme.colors.button.white};
+      props.theme.colors[props.colorBtHover] ?? props.theme.colors.whiteText};
     border-color: ${(props: any) =>
-      props.theme.colors.button[props.borderColorBtHover] ??
-      props.theme.colors.button.white};
+      props.theme.colors[props.borderColorBtHover] ??
+      props.theme.colors.whiteText};
     background-color: ${(props: any) =>
-      props.theme.backgrounds.button[props.backgroundColorBtHover] ??
-      props.theme.backgrounds.button.primaryEnabled};
+      props.theme.colors[props.backgroundColorBtHover] ??
+      props.theme.colors.primaryEnabledBt};
   }
   &:active {
-    color: ${(props: any) => props.theme.colors.button.white};
-    border-color: ${(props: any) => props.theme.colors.button.pink};
-    background-color: ${(props: any) =>
-      props.theme.backgrounds.button.primaryPressed};
+    color: ${(props: any) => props.theme.colors.whiteText};
+    border-color: ${(props: any) => props.theme.colors.pinkText};
+    background-color: ${(props: any) => props.theme.colors.primaryPressedBt};
   }
   &:disabled {
     opacity: 0.5;
     color: ${(props: any) =>
-      props.theme.colors.button[props.colorBt] ??
-      props.theme.colors.button.white};
+      props.theme.colors[props.colorBt] ?? props.theme.colors.whiteText};
     border-color: ${(props: any) =>
-      props.theme.colors.button[props.borderColorBt] ??
-      props.theme.colors.button.white};
+      props.theme.colors[props.borderColorBt] ?? props.theme.colors.whiteText};
     background-color: ${(props: any) =>
-      props.theme.backgrounds.button[props.backgroundColorBt] ??
-      props.theme.backgrounds.button.primaryEnabled};
+      props.theme.colors[props.backgroundColorBt] ??
+      props.theme.colors.primaryEnabledBt};
   }
 `;
