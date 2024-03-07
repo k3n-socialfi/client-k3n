7a9c0cdc3db87a582a36626a2fabc2ea5d16e842
curtis <curtisnguyen4@gmail.com>
2024-03-07 23:45:02 +0700

    [feature] [K3N-17] build common component card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,37 +4,42 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import styled from 'styled-components';
-import Chips from '../Chip';
-import { IconLike } from '@/assets/icons';
 
-const IMG_NFT = "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+interface ICards {
+    url: string,
+    title: string,
+    content: any,
+    actions: any
+}
 
-export default function Cards(props: any) {
+export default function Cards({
+    url, title, content, actions
+}: ICards) {
     return (
-        <Card sx={{ maxWidth: 345, background: "#252525" }} {...props}>
+        <CustomCard>
             <CardMedia
                 sx={{ height: 202 }}
-                image={IMG_NFT}
+                image={url}
                 title="green iguana"
             />
             <CardContent>
-                <StyleTitle>Title</StyleTitle>
+                <StyleTitle>{title}</StyleTitle>
                 <StyleChips>
-                    <Chips label="Social Fi" variant="outlined" />
-                    <Chips label="Researcher" color="success" />
-                    <Chips label="Ethereum" color="warning" />
+                    {content}
                 </StyleChips>
             </CardContent>
-            <CardActions sx={{paddingLeft: "16px"}}>
-                <Button sx={{ borderRadius: "14px" }} color="primary" variant="outlined" size="medium" startIcon={<IconLike />}>4.6M</Button>
+            <CardActions sx={{ paddingLeft: "16px" }}>
+                {actions}
             </CardActions>
-        </Card>
+        </CustomCard>
     )
 }
 
+const CustomCard = styled(Card)`
+    max-width: 345px;
+    background-color: #252525;
+`
 const StyleTitle = styled.div`
   font-size: 18px;
   padding-bottom: 12px;
