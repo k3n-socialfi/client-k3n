09062ade45ef9b8dd65560cd8fa17061676f48a6
curtis <curtisnguyen4@gmail.com>
2024-03-29 20:14:30 +0700

    [fix][K3N-_]:config link image

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "cafebiz.cafebizcdn.vn",
-          },
-          {
-            protocol: "https",
-            hostname: "images.pexels.com",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cafebiz.cafebizcdn.vn",
       },
-      compiler: {
-        styledComponents: true,
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
       },
+      {
+        protocol: "https",
+        hostname: "s3-alpha-sig.figma.com",
+      },
+    ],
+  },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
