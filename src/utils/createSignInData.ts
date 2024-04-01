659f9129106c3ca43958d02dcd5786ece6967447
curtis0799 <curtisnguyen4@gmail.com>
2024-04-01 22:11:39 +0700

    [feature] [K3N-0] config func createSignInData

--- src/utils/createSignInData.ts
+++ src/utils/createSignInData.ts
@@ -4,16 +4,17 @@ import { SolanaSignInInput } from "@solana/wallet-standard-features";
 export const createSignInData = async (address: any): Promise<SolanaSignInInput> => {
     // Convert the Date object to a string
     const messageSolana = await getMessageSolana(address);
-    const dataSignIn = JSON.parse(messageSolana.data.data)
-    const currentUrl = new URL(dataSignIn.uri);
+    const dataSignIn = JSON.parse(messageSolana.data.data);
+    const currentUrl = new URL(dataSignIn.domain);
     const domain = currentUrl.host;
     const signInData: SolanaSignInInput = {
-        domain,
         statement: dataSignIn.statement,
         version: dataSignIn.version,
-        nonce: dataSignIn.nonce,
         chainId: dataSignIn.chainId,
+        nonce: dataSignIn.nonce,
         issuedAt: dataSignIn.issuedAt,
+        expirationTime: dataSignIn.expirationTime,
+        resources: [domain],
     };
 
     return signInData;
