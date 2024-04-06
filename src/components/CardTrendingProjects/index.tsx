bce7a803610a97780c8bbf3daa34ef72076c0d06
funster <vovanphuong101098@gmail.com>
2024-04-06 09:36:48 +0700

    fix: custom cardtrendingproject and modified name module/project

--- src/components/CardTrendingProjects/index.tsx
+++ src/components/CardTrendingProjects/index.tsx
@@ -11,20 +11,20 @@ export default function CardTrendingProjects(props: any) {
       sx={{ width: "100%", background: "#252525", borderRadius: 3 }}
       {...props}
     >
-      <CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Stack
-          direction="row"
-          spacing={2}
-          sx={{ display: "flex", alignItems: "center" }}
-        >
-          <Avatar
-            alt={props?.name}
-            src={props?.avatar}
-            sx={{ width: 56, height: 56 }}
-          />
+      <LinkCustom href={`/project/${props?.id}`}>
+        <CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Stack
+            direction="row"
+            spacing={2}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Avatar
+              alt={props?.name}
+              src={props?.avatar}
+              sx={{ width: 56, height: 56 }}
+            />
 
-          <Stack direction="column">
-            <LinkCustom href={`/project/${props?.id}`}>
+            <Stack direction="column">
               <Typography
                 gutterBottom
                 variant="h5"
@@ -33,32 +33,32 @@ export default function CardTrendingProjects(props: any) {
               >
                 {props?.name}
               </Typography>
-            </LinkCustom>
-            <Stack
-              sx={{ display: "flex", gap: 1, alignItems: "end" }}
-              direction="row"
-            >
-              <ButtonText
-                size="small"
-                borderColorBt={"#FFD7F4"}
-                backgroundColorBt={"#FFD7F4"}
+              <Stack
+                sx={{ display: "flex", gap: 1, alignItems: "end" }}
+                direction="row"
               >
-                <Typography color={"#F23581"} sx={{ padding: "0 5px" }}>
-                  {props?.wallet}
+                <ButtonText
+                  size="small"
+                  borderColorBt={"#FFD7F4"}
+                  backgroundColorBt={"#FFD7F4"}
+                >
+                  <Typography color={"#F23581"} sx={{ padding: "0 5px" }}>
+                    {props?.wallet}
+                  </Typography>
+                </ButtonText>
+                <Typography
+                  gutterBottom
+                  variant="subtitle2"
+                  component="span"
+                  color={"gray"}
+                >
+                  #{props?.mention} mentions
                 </Typography>
-              </ButtonText>
-              <Typography
-                gutterBottom
-                variant="subtitle2"
-                component="span"
-                color={"gray"}
-              >
-                #{props?.mention} mentions
-              </Typography>
+              </Stack>
             </Stack>
           </Stack>
-        </Stack>
-      </CardContent>
+        </CardContent>
+      </LinkCustom>
     </Card>
   );
 }
