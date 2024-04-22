f3add817064127719a66ca015266342e6c4a2fa6
funster <vovanphuong101098@gmail.com>
2024-04-22 09:05:17 +0700

    fix: fixed path hook in context services

--- src/modules/services/context/ServicesContext.tsx
+++ src/modules/services/context/ServicesContext.tsx
@@ -1,6 +1,6 @@
 "use client";
-import useFetchDataServices from "@/modules/services/hook/useFetchDataServices";
 import React, { createContext, useContext } from "react";
+import useFetchDataServices from "../Hook/useFetchDataServices";
 
 interface IPropsServicesContextProvider {
   children: React.ReactNode;
