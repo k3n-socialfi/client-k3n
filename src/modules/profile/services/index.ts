623ed9f161baf01d8297964aded9299cc7f650f8
curtis0799 <curtisnguyen4@gmail.com>
2024-05-02 00:31:52 +0700

    intergation list services api

--- src/modules/profile/services/index.ts
+++ src/modules/profile/services/index.ts
@@ -1,1 +1,6 @@
 //service call api
+import axiosInstance from "@/configs/axios.config";
+
+export const getJobsUser = (username: string) => {
+    return axiosInstance.get(`/api/v1/jobs/list-offers/${username}`);
+};
